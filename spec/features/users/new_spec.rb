79498a5a0e67bf3a2faa1a465e36f8ef639e7fa2
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-01 13:24:02 -0600

    Update user registration feature test for separate address table

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -1,21 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe "User Registration form" do
-  it "can create new user" do
+  it "can create new user and their home address" do
     visit root_path
 
     click_on 'Register'
 
     expect(current_path).to eq('/register')
 
-    fill_in "user[name]", with: "Billy"
-    fill_in "user[address]", with: "123 go to santa lane"
-    fill_in "user[city]", with: "aurora"
-    fill_in "user[state]", with: "colorado"
-    fill_in "user[email]", with: "billyurrutia@gmail.com"
-    fill_in "user[zip]", with: "123311"
-    fill_in "user[password]", with: "1233"
-    fill_in "user[password_confirmation]", with: "1233"
+    name = "Billy"
+    street = "123 go to santa lane"
+    city = "aurora"
+    state = "colorado"
+    email = "billyurrutia@gmail.com"
+    zip = "123311"
+    password = "1233"
+
+    fill_in "user[name]", with: name
+    fill_in "user[address]", with: street
+    fill_in "user[city]", with: city
+    fill_in "user[state]", with: state
+    fill_in "user[email]", with: email
+    fill_in "user[zip]", with: zip
+    fill_in "user[password]", with: password
+    fill_in "user[password_confirmation]", with: password
 
     click_on "Register User"
 
@@ -23,7 +31,14 @@ RSpec.describe "User Registration form" do
 
     new_user = User.last
 
-    expect(page).to have_content("Welcome, #{new_user.name}")
+    expect(page).to have_content("Welcome, #{name}")
+    expect(new_user.name).to eq(name)
+    expect(new_user.email).to eq(email)
+    expect(new_user.primary_address.nickname).to eq("home")
+    expect(new_user.primary_address.street).to eq(street)
+    expect(new_user.primary_address.street).to eq(city)
+    expect(new_user.primary_address.street).to eq(state)
+    expect(new_user.primary_address.street).to eq(zip)
   end
 
   context "put in wrong confirmation password" do

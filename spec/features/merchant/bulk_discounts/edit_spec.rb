23c1c02c23072ae8765b56db6e9dafe078d492f8
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-03 17:36:11 -0600

    Add feature tests for bulk discount edit form (as a merchant)

--- spec/features/merchant/bulk_discounts/edit_spec.rb
+++ spec/features/merchant/bulk_discounts/edit_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'Bulk Discount New Form' do
+RSpec.describe 'Bulk Discount Edit Form' do
   describe 'as a merchant' do
     before :each do
       @merchant = create(:merchant)
       address = create(:address, user: @merchant)
+      @bulk_discount = create(:bulk_discount, user: @merchant)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
 
@@ -12,141 +13,149 @@ RSpec.describe 'Bulk Discount New Form' do
       @pc_off = 30.0
     end
 
-    it "has a form to add a new bulk discount" do
-      visit new_merchant_bulk_discount_path
+    it "has a form to edit an existing bulk discount" do
+      visit edit_merchant_bulk_discount_path(@bulk_discount)
 
       fill_in "bulk_discount[bulk_quantity]", with: @bulk_quantity
       fill_in "bulk_discount[pc_off]", with: @pc_off
 
-      click_button "Create Bulk Discount"
+      click_button "Update Bulk Discount"
 
       expect(current_path).to eq(merchant_bulk_discounts_path)
 
-      new_discount = BulkDiscount.last
-
-      expect(page).to have_content("A new bulk discount was added")
-      expect(new_discount.bulk_quantity).to eq(@bulk_quantity)
-      expect(new_discount.pc_off).to eq(@pc_off)
+      expect(page).to have_content("The bulk discount was updated")
+      expect(@bulk_discount.reload.bulk_quantity).to eq(@bulk_quantity)
+      expect(@bulk_discount.reload.pc_off).to eq(@pc_off)
     end
 
     it "requires bulk_quantity input" do
-      visit new_merchant_bulk_discount_path
+      visit edit_merchant_bulk_discount_path(@bulk_discount)
 
       # DON'T fill_in "bulk_discount[bulk_quantity]", with: @bulk_quantity
       fill_in "bulk_discount[pc_off]", with: @pc_off
 
       click_button "Create Bulk Discount"
 
-      expect(current_path).to eq(new_merchant_bulk_discount_path)
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(@bulk_discount))
       expect(page).to have_field("bulk_discount[bulk_quantity]")
       expect(page).to have_content("Bulk quantity is not a number")
-      expect(BulkDiscount.count).to eq(0)
+      expect(@bulk_discount.reload.bulk_quantity).to_not eq(@bulk_quantity)
+      expect(@bulk_discount.reload.pc_off).to_not eq(@pc_off)
     end
 
     it "bulk_quantity input must be 2 or greater" do
-      visit new_merchant_bulk_discount_path
+      visit edit_merchant_bulk_discount_path(@bulk_discount)
 
       fill_in "bulk_discount[bulk_quantity]", with: "hello"
       fill_in "bulk_discount[pc_off]", with: @pc_off
 
       click_button "Create Bulk Discount"
 
-      expect(current_path).to eq(new_merchant_bulk_discount_path)
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(@bulk_discount))
       expect(page).to have_field("bulk_discount[bulk_quantity]")
       expect(page).to have_content("Bulk quantity is not a number")
-      expect(BulkDiscount.count).to eq(0)
+      expect(@bulk_discount.reload.bulk_quantity).to_not eq(@bulk_quantity)
+      expect(@bulk_discount.reload.pc_off).to_not eq(@pc_off)
 
       fill_in "bulk_discount[bulk_quantity]", with: "-5"
       fill_in "bulk_discount[pc_off]", with: @pc_off
 
       click_button "Create Bulk Discount"
 
-      expect(current_path).to eq(new_merchant_bulk_discount_path)
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(@bulk_discount))
       expect(page).to have_field("bulk_discount[bulk_quantity]")
       expect(page).to have_content("Bulk quantity must be greater than or equal to 2")
-      expect(BulkDiscount.count).to eq(0)
+      expect(@bulk_discount.reload.bulk_quantity).to_not eq(@bulk_quantity)
+      expect(@bulk_discount.reload.pc_off).to_not eq(@pc_off)
 
       fill_in "bulk_discount[bulk_quantity]", with: 1
       fill_in "bulk_discount[pc_off]", with: @pc_off
 
       click_button "Create Bulk Discount"
 
-      expect(current_path).to eq(new_merchant_bulk_discount_path)
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(@bulk_discount))
       expect(page).to have_field("bulk_discount[bulk_quantity]")
       expect(page).to have_content("Bulk quantity must be greater than or equal to 2")
-      expect(BulkDiscount.count).to eq(0)
+      expect(@bulk_discount.reload.bulk_quantity).to_not eq(@bulk_quantity)
+      expect(@bulk_discount.reload.pc_off).to_not eq(@pc_off)
 
       fill_in "bulk_discount[bulk_quantity]", with: 4.5
       fill_in "bulk_discount[pc_off]", with: @pc_off
 
       click_button "Create Bulk Discount"
 
-      expect(current_path).to eq(new_merchant_bulk_discount_path)
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(@bulk_discount))
       expect(page).to have_field("bulk_discount[bulk_quantity]")
       expect(page).to have_content("Bulk quantity must be an integer")
-      expect(BulkDiscount.count).to eq(0)
+      expect(@bulk_discount.reload.bulk_quantity).to_not eq(@bulk_quantity)
+      expect(@bulk_discount.reload.pc_off).to_not eq(@pc_off)
     end
 
     it "requires pc_off input" do
-      visit new_merchant_bulk_discount_path
+      visit edit_merchant_bulk_discount_path(@bulk_discount)
 
       fill_in "bulk_discount[bulk_quantity]", with: @bulk_quantity
       # DON'T fill_in "bulk_discount[pc_off]", with: @pc_off
 
       click_button "Create Bulk Discount"
 
-      expect(current_path).to eq(new_merchant_bulk_discount_path)
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(@bulk_discount))
       expect(page).to have_field("bulk_discount[bulk_quantity]")
       expect(page).to have_content("Pc off is not a number")
-      expect(BulkDiscount.count).to eq(0)
+      expect(@bulk_discount.reload.bulk_quantity).to_not eq(@bulk_quantity)
+      expect(@bulk_discount.reload.pc_off).to_not eq(@pc_off)
     end
 
     it "pc_off input must be 0.01 or greater" do
-      visit new_merchant_bulk_discount_path
+      visit edit_merchant_bulk_discount_path(@bulk_discount)
 
       fill_in "bulk_discount[bulk_quantity]", with: @bulk_quantity
       fill_in "bulk_discount[pc_off]", with: "hello"
 
       click_button "Create Bulk Discount"
 
-      expect(current_path).to eq(new_merchant_bulk_discount_path)
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(@bulk_discount))
       expect(page).to have_field("bulk_discount[bulk_quantity]")
       expect(page).to have_content("Pc off is not a number")
-      expect(BulkDiscount.count).to eq(0)
+      expect(@bulk_discount.reload.bulk_quantity).to_not eq(@bulk_quantity)
+      expect(@bulk_discount.reload.pc_off).to_not eq(@pc_off)
 
       fill_in "bulk_discount[bulk_quantity]", with: @bulk_quantity
       fill_in "bulk_discount[pc_off]", with: "-3.2"
 
       click_button "Create Bulk Discount"
 
-      expect(current_path).to eq(new_merchant_bulk_discount_path)
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(@bulk_discount))
       expect(page).to have_field("bulk_discount[bulk_quantity]")
       expect(page).to have_content("Pc off must be greater than or equal to 0.01")
-      expect(BulkDiscount.count).to eq(0)
+      expect(@bulk_discount.reload.bulk_quantity).to_not eq(@bulk_quantity)
+      expect(@bulk_discount.reload.pc_off).to_not eq(@pc_off)
 
       fill_in "bulk_discount[bulk_quantity]", with: @bulk_quantity
       fill_in "bulk_discount[pc_off]", with: 0.0
 
       click_button "Create Bulk Discount"
 
-      expect(current_path).to eq(new_merchant_bulk_discount_path)
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(@bulk_discount))
       expect(page).to have_field("bulk_discount[bulk_quantity]")
       expect(page).to have_content("Pc off must be greater than or equal to 0.01")
-      expect(BulkDiscount.count).to eq(0)
+      expect(@bulk_discount.reload.bulk_quantity).to_not eq(@bulk_quantity)
+      expect(@bulk_discount.reload.pc_off).to_not eq(@pc_off)
     end
 
     it "pc_off input must be 99.99 or less" do
-      visit new_merchant_bulk_discount_path
+      visit edit_merchant_bulk_discount_path(@bulk_discount)
 
       fill_in "bulk_discount[bulk_quantity]", with: @bulk_quantity
       fill_in "bulk_discount[pc_off]", with: 100
 
       click_button "Create Bulk Discount"
 
-      expect(current_path).to eq(new_merchant_bulk_discount_path)
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(@bulk_discount))
       expect(page).to have_field("bulk_discount[bulk_quantity]")
       expect(page).to have_content("Pc off must be less than or equal to 99.99")
-      expect(BulkDiscount.count).to eq(0)
+      expect(@bulk_discount.reload.bulk_quantity).to_not eq(@bulk_quantity)
+      expect(@bulk_discount.reload.pc_off).to_not eq(@pc_off)
     end
   end
 end

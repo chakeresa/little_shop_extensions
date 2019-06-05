c7d45c23ce44bd623ababa1ce86649550fb9a417
Alexandra Chakeres <alexchakeres@gmail.com>
2019-06-05 10:01:06 -0600

    Combine 2 similar test files for changing item amounts in the cart

--- spec/features/carts/add_or_remove_items_spec.rb
+++ spec/features/carts/add_or_remove_items_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "adding items to their cart" do
-  context "as a visitor" do
+RSpec.describe "adding or removing items from their cart" do
+  context "as a visitor with an empty cart" do
     before(:each) do
       @merchant_1 = create(:merchant)
       @item_1 = create(:item, user: @merchant_1, name: "Sofa")
       @item_2 = create(:item, user: @merchant_1, name: "Chair")
     end
 
-    it "displays a message" do
+    it "displays a message when I add an item to my cart" do
       visit item_path(@item_1)
       click_button "Add to Cart"
 
@@ -50,6 +50,76 @@ RSpec.describe "adding items to their cart" do
     end
   end
 
+  context "as a visitor with items in my cart" do
+    before :each do
+      @merchant_1 = create(:merchant)
+      @item_1 = create(:item, user: @merchant_1, name: "Sofa", inventory: 3)
+      @item_2 = create(:item, user: @merchant_1, name: "Chair", inventory: 2)
+
+      visit item_path(@item_1)
+      click_button "Add to Cart"
+
+      visit item_path(@item_2)
+      click_button "Add to Cart"
+
+      visit item_path(@item_1)
+      click_button "Add to Cart"
+    end
+
+    it 'I see buttons to increment/decrement the quantities in my cart' do
+      visit cart_path
+
+      expect(page).to have_button("+")
+      expect(page).to have_button("-")
+      expect(page).to have_button("Remove")
+
+      within "#item-#{@item_1.id}" do
+        click_button "+"
+      end
+
+      expect(page).to have_content("You now have #{pluralize(3, @item_1.name)} in your cart")
+
+      within "#item-#{@item_1.id}" do
+        click_button "+"
+        expect(page).to have_content(@item_1.inventory)
+      end
+
+      expect(page).to have_content("Merchant does not have any more #{@item_1.name}")
+    end
+
+    it 'I see a button to remove an item completely from my cart' do
+      visit cart_path
+
+      within "#item-#{@item_2.id}" do
+        click_button "Remove"
+      end
+
+      expect(page).to_not have_link("#{@item_2.name}")
+      expect(page).to have_content("You have removed #{@item_2.name} from your cart")
+    end
+
+    it 'there is a button to decrement an item by 1' do
+      visit item_path(@item_1)
+      click_button "Add to Cart"
+
+      visit cart_path
+
+      within "#item-#{@item_1.id}" do
+        click_button "-"
+        expect(page).to have_content(@item_1.inventory - 1)
+      end
+
+      expect(page).to have_content("You have removed 1 #{@item_1.name} from your cart.")
+
+      within "#item-#{@item_1.id}" do
+        click_button "-"
+        click_button "-"
+      end
+
+      expect(page).to_not have_link("#{@item_1.name}")
+    end
+  end
+
   context "as a user" do
     before(:each) do
       @merchant_1 = create(:merchant)
@@ -60,7 +130,7 @@ RSpec.describe "adding items to their cart" do
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
     end
 
-    it "displays a message" do
+    it "displays a message when I add an item to my cart" do
       visit item_path(@item_1)
       click_button "Add to Cart"
 
